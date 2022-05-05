87bc46fd817ec6b32e0047d4bb51595d12bcb7fb
RameshMalli573 <61266298+RameshMalli573@users.noreply.github.com>
2022-05-05 22:50:44 +0530

    Update FitnessController.java

--- fitness-tracker/src/main/java/com/config/FitnessController.java
+++ fitness-tracker/src/main/java/com/config/FitnessController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/fitnesstracker")
+@RequestMapping("/api/v1.0/fitnesstracker")
 public class FitnessController {
 	
 	@Autowired FitnessService service;
